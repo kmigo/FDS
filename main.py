b90b23385de55fb0c2734f3d1ca4dfaa72e292ac
Patrick Soares <tk_patrick@hotmail.com>
2018-12-22 21:55:00 -0200

    Criando Toolbar

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #coding=utf-8
+
+from kivy.app import App
+from kivy.uix.screenmanager import Screen,ScreenManager
+
+
+class GerenciadorTela(ScreenManager):
+	pass
+
+
+class Tela(Screen):
+	pass
+	
+
+class Aplicativo(App):
+	def build(self):
+		return GerenciadorTela()
+
+Aplicativo().run()
